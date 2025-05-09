ae48521f8ec0ed996b1fbb3aa1deeb56e1f930e8
Kenth Mariano <kjsamonte27@gmail.com>
2025-05-09 13:43:26 +0800

    Update SFML PRACTICE BRUH.cpp

--- SFML PRACTICE BRUH.cpp
+++ SFML PRACTICE BRUH.cpp
@@ -7,23 +7,52 @@ using namespace std;
 using namespace sf;
 
 int main() {
-	VideoMode desktopMODE = VideoMode::getDesktopMode();
-	RenderWindow window(desktopMODE, "Practice");
-	Event yolo;
+	VideoMode desktop = VideoMode::getDesktopMode();
+	RenderWindow window(desktop, "PRACTICE GAME");
+	Event ev;
+	CircleShape bilog(80);
+	RectangleShape parihaba(Vector2f(150, 75));
+	bilog.setFillColor(Color::Yellow);
+	bilog.setPosition(855, 410);
+	parihaba.setFillColor(Color::Cyan);
+	parihaba.setPosition(855, 410);
+	float speed = 3.0f;
 
 	while (window.isOpen()) {
-		while (window.pollEvent(yolo)) {
-			switch (yolo.type) {
+		while (window.pollEvent(ev)) {
+			switch (ev.type) {
 			case Event::Closed:
 				window.close();
 				break;
 			case Event::KeyPressed:
-				if (yolo.key.code == Keyboard::Escape)
+				if (ev.key.code == Keyboard::Escape)
 					window.close();
 				    break;
 			}
 		}
-		window.clear(Color::Yellow);
+
+		if (Keyboard::isKeyPressed(Keyboard::Right))
+			bilog.move(speed, 0);
+		if (Keyboard::isKeyPressed(Keyboard::Left))
+			bilog.move(-speed, 0);
+		if (Keyboard::isKeyPressed(Keyboard::Up))
+			bilog.move(0, -speed);
+		if (Keyboard::isKeyPressed(Keyboard::Down))
+			bilog.move(0, speed);
+
+		if (Keyboard::isKeyPressed(Keyboard::D))
+			parihaba.move(speed, 0);
+		if (Keyboard::isKeyPressed(Keyboard::A))
+			parihaba.move(-speed, 0);
+		if (Keyboard::isKeyPressed(Keyboard::W))
+			parihaba.move(0, -speed);
+		if (Keyboard::isKeyPressed(Keyboard::S))
+			parihaba.move(0, speed);
+
+
+		window.clear();
+		window.draw(bilog);
+		window.draw(parihaba);
 		window.display();
 	}
 }
